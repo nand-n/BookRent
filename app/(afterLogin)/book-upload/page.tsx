0486b7aeafc8c6ec2c9c1434fc1c0c10b079a31e
Nahi002 <nahomdebele002@gmail.com>
2024-08-12 23:30:19 +0300

    book upload

--- app/(afterLogin)/book-upload/page.tsx
+++ app/(afterLogin)/book-upload/page.tsx
@@ -1,81 +1,255 @@
 'use client';
 import { useBookGetsByAdmin } from '@/store/server/features/books/queries';
-import React, { useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import BookUploadForm from './_components/bookForm';
+import { Form, Input, Select, Button, Upload } from 'antd';
+import { AiFillEdit, AiFillDelete } from 'react-icons/ai';
+import clsx from 'clsx';
+import Image from 'next/image';
+import { UploadFile } from 'antd/lib';
+import { AiOutlineCloudUpload } from 'react-icons/ai';
+import { useBookPublish } from '@/store/uistate/features/bookPublish/useStore';
+import {
+  useAddBook,
+  usePublishBook,
+} from '@/store/server/features/publishBook/mutation';
+import CustomSuccessModal from '@/components/common/sucessModal/successModal';
 
-function BookUplad() {
+function BookUpload() {
   const { data: booksCreatedByadmins } = useBookGetsByAdmin();
-  const [dropDown, setDropDown] = useState<boolean>(false);
   const [visible, setVisible] = useState<boolean>(false);
-  const [selectedBook, setSelectedBook] = useState<string>('');
+  const [successVisible, setSuccessVisible] = useState<boolean>(false);
+
+  const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
+
+  const { Option } = Select;
+  const [form] = Form.useForm();
+
+  const {
+    coverImage,
+    quantity,
+    rentPrice,
+    setCoverImage,
+    setQuantity,
+    setRentPrice,
+    selectedBookId,
+    setSelectedBook,
+  } = useBookPublish();
+  const {
+    mutate: publishBook,
+    isSuccess,
+    isLoading: isSubmitLoading,
+  } = usePublishBook();
+  const { mutate: addBook, isLoading: isAddbookLoading } = useAddBook();
+
+  const onSubmit = () => {
+    form.validateFields();
+    const data = {
+      id: selectedBookId,
+      body: {
+        files: coverImage?.originFileObj,
+        quantity,
+        price: rentPrice,
+      },
+    };
+    publishBook(data);
+  };
+  const onAddSubmit = (value: any) => {
+    addBook(value);
+  };
+  useEffect(() => {
+    if (isSuccess == true) {
+      setSuccessVisible(true);
+    }
+  }, [isSuccess]);
+
+  const normFile = (e: any) => {
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e?.fileList;
+  };
+
+  const handleFileChange = (info: any) => {
+    const fileList = info.fileList as UploadFile<any>[];
+    if (fileList.length > 0) {
+      setCoverImage(fileList[0]);
+    } else {
+      setCoverImage(undefined);
+    }
+  };
+
+  const getImageUrl = (file: UploadFile<any> | undefined) => {
+    if (file) {
+      if (file.url) {
+        return file.url;
+      }
+      if (file.originFileObj) {
+        return URL.createObjectURL(file.originFileObj);
+      }
+    }
+    return '';
+  };
 
   return (
-    <div className="py-4 grid justify-center items-center">
-      <div className="text-center mb-4">
-        <h2 className="text-xl font-semibold">Upload New Book</h2>
+    <div className="py-8 flex flex-col items-center bg-white w-full min-h-screen">
+      <div className="text-center mb-6">
+        <h2 className="text-2xl font-semibold">Upload New Book</h2>
+      </div>
+      <div className="relative w-96 mb-6 rounded-t-2xl bg-gray-200">
+        <div className=" text-gray-500 p-2 ">Search book by name or author</div>
+
+        <Select
+          showSearch
+          value={selectedBookId}
+          onChange={(value) => setSelectedBook(value)}
+          placeholder="Search..."
+          className={clsx(
+            'w-full border-2 text-black',
+            dropdownOpen ? ' border-b-blue' : 'border-b-black',
+          )}
+          onDropdownVisibleChange={(open) => setDropdownOpen(open)}
+          filterOption={(input, option: any) =>
+            option?.children.toLowerCase().includes(input.toLowerCase())
+          }
+          dropdownRender={(menu) => (
+            <>
+              {menu}
+              <div
+                className="border-t px-4 py-2 text-blue-500 hover:bg-gray-100 cursor-pointer"
+                onClick={() => setVisible(true)}
+              >
+                Add
+              </div>
+            </>
+          )}
+        >
+          {booksCreatedByadmins?.map((book, index) => (
+            <Option key={index} value={book.id}>
+              {book.name}
+            </Option>
+          ))}
+        </Select>
       </div>
 
-      <div className="relative w-96 h-96  overflow-y-auto">
-        <div
-          className="border-1 border-b-black rounded-md flex justify-between items-center px-2 py-1 cursor-pointer bg-gray-300 "
-          onClick={() => setDropDown(!dropDown)}
-        >
-          <div className="">
-            <div className="">
-              {' '}
-              <div className="text-gray-400 font-light">
-                Search book by name or author
-              </div>
-              <div className="p-0 border-b-black">
-                <input
-                  value={selectedBook}
-                  type="text"
-                  placeholder="Search..."
-                  className="w-full  rounded-md p-2 outline-none bg-transparent"
-                />
-              </div>
-              {dropDown && (
-                <div className="absolute w-full bg-white shadow-lg border mt-1 rounded-md z-10 ">
-                  <div className="max-h-48 overflow-y-auto py-2">
-                    {booksCreatedByadmins?.map(({ name }, index) => (
-                      <div
-                        key={index}
-                        className="px-4 py-2 cursor-pointer hover:bg-gray-100"
-                        onClick={() => {
-                          setSelectedBook(name);
-                          setDropDown(false);
-                        }}
-                      >
-                        {name}
-                      </div>
-                    ))}
-                  </div>
-                  <div className="pb-4 pt-2 border-t-2">
-                    <div
-                      className=" px-4 py-2 cursor-pointer text-blue-500  text-primary hover:bg-gray-100"
-                      onClick={() => setVisible(true)}
-                    >
-                      Add
-                    </div>
-                  </div>
-                </div>
-              )}
-            </div>
+      <div className="w-96 mb-6">
+        <Form layout="vertical" form={form} onFinish={onSubmit}>
+          <div className="flex justify-between gap-4">
+            <Form.Item
+              name="quantity"
+              label="Book Quantity"
+              className="w-full"
+              rules={[{ required: true, message: 'Please select quantity' }]}
+            >
+              <Select
+                onSelect={(e) => setQuantity(Number(e))}
+                placeholder="Book Quantity"
+                className="h-12"
+              >
+                {Array.from({ length: 1000 }, (_, index) => (
+                  <Option key={index} value={index + 1}>
+                    {index + 1}
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
+
+            <Form.Item
+              name="price"
+              label="Rent price for 2 weeks"
+              className="w-full"
+              rules={[{ required: true, message: 'Please enter book price' }]}
+            >
+              <Input
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  setRentPrice(Number(e.target.value))
+                }
+                type="number"
+                size="large"
+                placeholder="Rent price for 2 weeks"
+                className="w-full h-12"
+              />
+            </Form.Item>
           </div>
-          <div>{dropDown ? '▲' : '▼'}</div>
-        </div>
+          <Form.Item
+            id="coverImage"
+            name="coverImage"
+            label="Book Cover Image"
+            rules={[
+              { required: true, message: 'Please uplaod book cover image!' },
+            ]}
+            className={'w-full font-bold text-4xl mt-4'}
+          >
+            <Form.Item
+              label="Upload Book Cover"
+              className="w-full font-normal text-xl mt-4"
+            >
+              <Form.Item
+                valuePropName="fileList"
+                getValueFromEvent={normFile}
+                noStyle
+                className="mt-2 w-full font-normal text-xl"
+              >
+                <Upload.Dragger
+                  name="files"
+                  className="mt-2"
+                  onChange={handleFileChange}
+                  fileList={coverImage ? [coverImage] : []}
+                  showUploadList={false}
+                  accept="image/*"
+                  maxCount={1}
+                >
+                  {coverImage ? (
+                    <div className="mt-4">
+                      <Image
+                        width={300}
+                        height={300}
+                        src={getImageUrl(coverImage)}
+                        alt="Uploaded Preview"
+                        className="w-full h-auto max-h-64 object-cover rounded-xl"
+                      />
+                    </div>
+                  ) : (
+                    <>
+                      <p className="text-5xl flex justify-center text-primary ">
+                        <AiOutlineCloudUpload />
+                      </p>
+                      <p className="ant-upload-text">Upload book cover</p>
+                      <p className="ant-upload-hint">
+                        or drag and drop it here
+                      </p>
+                      <p className="ant-upload-hint">Square 300 x 300 px</p>
+                    </>
+                  )}
+                </Upload.Dragger>
+              </Form.Item>
+            </Form.Item>
+          </Form.Item>
+          <Button
+            loading={isSubmitLoading}
+            type="primary"
+            className="w-full h-12"
+            htmlType="submit"
+          >
+            Submit
+          </Button>
+        </Form>
       </div>
 
       <BookUploadForm
         onClose={() => setVisible(false)}
         open={visible}
-        submitAction={(value) => {
-          console.log(value);
-        }}
+        submitAction={onAddSubmit}
         title="Add Book"
+        loading={isAddbookLoading}
+      />
+      <CustomSuccessModal
+        onClose={() => setSuccessVisible(false)}
+        visible={successVisible}
+        text="Your have uploaded the book successfully. Waite until we approved it."
       />
     </div>
   );
 }
 
-export default BookUplad;
+export default BookUpload;
