ce9ea8807c4b891c5d9be3cc6d45e6884016ae08
Nahi002 <nahomdebele002@gmail.com>
2024-08-12 23:33:06 +0300

    reactquery provider

--- providers/reactQueryProvider.tsx
+++ providers/reactQueryProvider.tsx
@@ -1,9 +1,9 @@
 'use client';
-
 import { QueryCache, QueryClient, QueryClientProvider } from 'react-query';
 import { ReactNode } from 'react';
-import NotificationMessage from '@/components/common/notification/notificationMessage';
 import { useRouter } from 'next/navigation';
+import { handleNetworkError } from '@/utils/showErrorResponse';
+import { handleSuccessMessage } from '@/utils/showSuccessMessage';
 
 /**
  * Interface for the props of the ReactQueryWrapper component
@@ -24,22 +24,30 @@ const ReactQueryWrapper: React.FC<ReactQueryWrapperProps> = ({ children }) => {
   const router = useRouter();
 
   const queryClient = new QueryClient({
-    queryCache: new QueryCache({
-      onError(error: any, query) {
-        if (error.response) {
-          if (error.response.status === 401) {
+    defaultOptions: {
+      mutations: {
+        onError(error: any) {
+          if (error?.response?.status === 401) {
             router.replace('/authentication/login');
           }
-          NotificationMessage.error({
-            message: 'Error',
-            description: error.response.data.message,
-          });
-        } else {
-          NotificationMessage.error({
-            message: 'Error',
-            description: error.message,
-          });
+          handleNetworkError(error);
+        },
+        onSuccess: (data: any, variables: any, context: any) => {
+          console.log(variables, context);
+          const method =
+            context?.method?.toUpperCase() || variables?.method?.toUpperCase();
+          const customMessage = context?.customMessage || undefined;
+
+          handleSuccessMessage(method, customMessage);
+        },
+      },
+    },
+    queryCache: new QueryCache({
+      onError(error: any) {
+        if (error?.response?.status === 401) {
+          router.replace('/authentication/login');
         }
+        handleNetworkError(error);
       },
     }),
   });
