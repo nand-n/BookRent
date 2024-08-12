0dfedbd23ec9f972747323e7e8c7dfc6df364cff
Nahi002 <nahomdebele002@gmail.com>
2024-08-12 23:32:37 +0300

    success modal

---
+++ components/common/sucessModal/successModal.tsx
@@ -0,0 +1,53 @@
+'use client';
+import { Button, Modal, Typography } from 'antd';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+
+const { Text } = Typography;
+
+interface CustomSuccessModalProps {
+  visible: boolean;
+  onClose: () => void;
+  text: string;
+}
+
+const CustomSuccessModal: React.FC<CustomSuccessModalProps> = ({
+  visible,
+  onClose,
+  text,
+}) => {
+  return (
+    <Modal open={visible} footer={null} onCancel={onClose} centered>
+      <div style={{ textAlign: 'center' }} className="grid ">
+        <div className="flex justify-center  items-center">
+          <Image
+            className=""
+            src="/icons/smile.svg"
+            alt="Success"
+            width={200}
+            height={200}
+          />
+        </div>
+
+        <Text className="mt-4 font-bold text-2xl">Congrats</Text>
+
+        <Text className="mt-4 font-bold text-sm text-gray-400 mb-4">
+          {text}
+        </Text>
+        <div className=" w-full flex justify-center items-center">
+          <Button
+            className="max-w-20 px-12 h-12"
+            type="primary"
+            onClick={() => onClose()}
+          >
+            {' '}
+            Ok
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default CustomSuccessModal;
