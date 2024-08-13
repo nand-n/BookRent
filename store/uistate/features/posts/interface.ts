c786311fa0fd4a93f4e82997e6b3161a8d536def
Nahi002 <nahomdebele002@gmail.com>
2024-08-13 21:03:17 +0300

    removed unwanted

--- store/uistate/features/posts/interface.ts
+++
@@ -1,8 +0,0 @@
-export interface CounterSlice {
-    counter:number 
-    incrementCounter : ()=> void
-    decrementCounter : ()=> void
-}
-
-export type MyState = CounterSlice
-
--- store/uistate/features/posts/useStore.ts
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-import { MyState } from "./interface";
-import createCounterSlice from "./createCounterSlice";
-
-const usePostStore = create<MyState>((set , get) =>({
-    ...createCounterSlice(set , get)
-}))
-
-export default usePostStore
